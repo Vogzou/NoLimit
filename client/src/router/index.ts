3dc85ec7e63707f8045ed280652174e41e933b41
val <valentin.bonnefon@diiage.org>
2024-04-21 21:22:40 +0200

    Add : Config

---
+++ client/src/router/index.ts
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import BoardView from "@/views/BoardView.vue";
+import HomeView from "@/views/HomeView.vue";
+import LobbyView from "@/views/LobbyView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/game-board/:roomId/:socketId',
+      name: 'board',
+      component: BoardView,
+      props : {
+        roomId : String,
+        socketId : String
+      }
+    },
+    {
+      path: '/lobby/:roomId/:socketId',
+      name: 'lobby',
+      component: LobbyView,
+      props : {
+        roomId : String,
+        socketId : String
+      }
+    }
+  ]
+})
+
+export default router
